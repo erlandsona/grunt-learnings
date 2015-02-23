0eb941957d1ac6062ad508ef53c40361ad38f2f5
Austin Erlandson <austin@erlandson.com>
2015-02-23 15:13:35 -0600

    added jade compile functionality

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,9 @@ module.exports = function (grunt) {
   // Load the plugin that provides the "copy" task.
   grunt.loadNpmTasks('grunt-contrib-copy');
 
+  // Load the plugin that provides the "jade compile" task.
+  grunt.loadNpmTasks('grunt-contrib-jade');
+
   // Project configuration.
   grunt.initConfig({
     copy: {
@@ -13,6 +16,11 @@ module.exports = function (grunt) {
         files: [
           {expand: true, cwd: 'app/', src: ['**'], dest: 'public/', filter: 'isFile'}
         ]
+      }
+    },
+    jade: {
+      compile: {
+        files: [{expand: true, cwd: 'app/', src: ['**/*.jade'], dest: 'public/', ext: '.html'}]
       }
     }
   });
